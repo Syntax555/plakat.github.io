bf9b9837c4834bb12e3ac3599b9be52a4c09b1ae
Tolga <github.com.police355@passmail.net>
2025-11-02 12:23:17 +0100

    Fix dynamic map loading in app router

--- my-nextjs-app/src/app/page.tsx
+++ my-nextjs-app/src/app/page.tsx
@@ -1,13 +1,4 @@
-import dynamic from "next/dynamic";
-
-const MapView = dynamic(() => import("./map-view").then((module) => module.MapView), {
-  ssr: false,
-  loading: () => (
-    <div className="flex h-[70vh] w-full items-center justify-center rounded-lg border border-dashed border-zinc-300 bg-white text-sm text-zinc-500">
-      Karte wird geladen...
-    </div>
-  ),
-});
+import MapView from "./map-view.client";
 
 export default function Home() {
   const currentYear = new Date().getFullYear();
